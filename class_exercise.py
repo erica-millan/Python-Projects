84f8b78cb2069120d432a0354c77820f3a646928
Erica Millan <ericamillan1234@gmail.com>
2025-11-17 12:36:46 -0600

    python inheritance assignment

---
+++ class_exercise.py
@@ -0,0 +1,17 @@
+class Animal:
+    #Define the attributes of the class
+    species = ""
+    sound = ""
+    diet = ""
+    habitat = ""
+
+    #Reptile class inherits from animal
+class Reptile(Animal):
+    scale_texture = ""
+    env_tempurature = 0
+
+    #Bird class inherits from animal
+class Bird(Animal):
+    wing_span = 0
+    beak_shape = ""
+    
